87236c1cc67a6e4b5d53c946658251e5edb761df
Joanna Podsadna <podsadnajoanna@gmail.com>
2020-07-06 11:38:09 +0200

    removed get_data_dau

--- src/utilitis.py
+++ src/utilitis.py
@@ -33,38 +33,3 @@ def make_date_3_month_early(current_date):
 
 
 
-# generate each  of metrics separatly
-
-def get_data_dau():
-    import pandas as pd
-
-
-    query = '''
-
-
-        select `date` as dzien,'poprzedni' as rok , metric_id as metric_id, tags['project_clone'] as project_clone, 
-        concat('poprzedni',' ', tags['project_clone']) as tag, value as value
-        from metrics.metrics_table
-        where tags['gg_id'] = '0' and (tags['project_clone'] = 'project_portal' or tags['project_clone'] = 'project_clones') 
-        and `date` >= '{}' and `date` < '{}' and `range` = 1
-        and metric_id = 'dau'
-
-         union 
-        select `date` as dzien,'obecny' as rok ,  metric_id as metric_id, tags['project_clone'] as project_clone, 
-        concat('obecny',' ', tags['project_clone']) as tag,value as value
-        from metrics.metrics_table
-        where tags['gg_id'] = '0' and (tags['project_clone'] = 'project_portal' or tags['project_clone'] = 'project_clones') 
-        and `date` >= '{}' and `date` < '{}' and `range` = 1
-        and metric_id in = 'dau'
-
-             '''.format(start_date_1, end_date_1, start_date_2, end_date_2)
-
-    gauss = get_hive_connection()
-
-    df = pd.read_sql(query, gauss)
-    # zrobic post _process _ data ....
-    df = df.rename(columns={"_u2.dzien": "dzien", "_u2.rok": "rok",
-                            "_u2.metric_id": "metric_id", "_u2.project_clone": "project_clone", "_u2.tag": "legenda",
-                            "_u2.value": "value"})
-
-    return df
